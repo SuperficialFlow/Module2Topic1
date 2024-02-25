0f83d6832a37ccfcf34cf9c4d5949c13e2992d1a
SuperficialFlow <154480145+SuperficialFlow@users.noreply.github.com>
2024-02-25 21:17:24 +0800

    revised

--- Module2Topic1/Exercise5.Master.designer.cs
+++ Module2Topic1/Exercise5.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Module2Topic1
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Content control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder Content;
-
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
